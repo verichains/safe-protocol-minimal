06b87242ed15d4092b4836c17ea495d9a8622c16
vhnvn <vh@fake.com>
2025-02-22 03:42:46 +0700

    Add NodeGlobalsPolyfillPlugin to vite config
    - Added `NodeGlobalsPolyfillPlugin` to the `optimizeDeps` section of the Vite configuration file.
    - This plugin helps polyfill node globals in ESBuild, which is used by Vite for bundling.
    - The plugin was added to ensure compatibility with the new `globalThis` variable introduced in ES6.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import sveltePreprocess from 'svelte-preprocess';
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill'
 
 export default defineConfig({
   base: '/safe-protocol-minimal/',  // Repository name
@@ -12,7 +13,17 @@ export default defineConfig({
     preprocess: sveltePreprocess()
   })],
   optimizeDeps: {
-    include: ['@safe-global/protocol-kit', 'ethers']
+    include: ['@safe-global/protocol-kit', 'ethers'],
+    esbuildOptions: {
+      define: {
+        global: 'globalThis'
+      },
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true
+        })
+      ]
+    }
   },
   define: {
     'process.env': {}
